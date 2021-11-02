ccff881f4500081894a0d304ab296d57f5323346
asgersong <asger.poulsen@yahoo.com>
2021-11-02 14:20:18 +0100

    implemented recursize function

--- src/search.c
+++ src/search.c
@@ -1,7 +1,17 @@
 #include "search.h"
 #include <stdbool.h>
+#include <assert.h>
 
 bool search(int a[], int n, int x)
 {
-    return 0;
+    assert (n >= 0); // precondition
+
+    if (n == 0) // simple case #1
+        return false;
+
+    else if (n > 0 && a[n-1] == x) // simple case #2
+        return true;
+
+    else // recursive step
+        return search(a, n-1, x);
 }
--- src/sum.c
+++ src/sum.c
@@ -1,6 +1,12 @@
 #include "sum.h"
+#include <assert.h>
 
 int sum(int a[], int n)
 {
-    return 0;
+    assert (n >= 0); // precondition
+
+    if (n == 0) // simple case
+        return 0;
+    else // recursive step
+        return a[n-1] + sum(a,n-1);
 }
