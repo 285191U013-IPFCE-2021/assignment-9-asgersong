272f245a257ff0eb6c411669a3e90839c10a973f
asgersong <asger.poulsen@yahoo.com>
2021-11-07 18:08:38 +0100

    Implemented DFS

--- src/dfs.c
+++ src/dfs.c
@@ -4,24 +4,65 @@
 
 #include <stdio.h>		/* scanf, printf */
 #include <stdlib.h>		/* abort */
-#include <stdbool.h>		/* bool, true, false */
-#include "dfs.h"
+#include <assert.h>   /* assert */
+#include <stdbool.h>	/* bool, true, false */
+#include "../include/dfs.h"
 
 
 void DFT (node * root)
 {
 	// Implement DFS
 	// Hint: You can use print_node, print_tree and/or print_stack.
+
+  // initialising 
+  stack *topp = NULL;
+  node *temp = NULL;
+
+  topp = push(topp, root); // topp is set to topp of our tree
+
+  
+  while (isEmpty(topp) == false) //while stack is not empty
+  {
+    temp = top(topp); // temporary node-pointer is set to point to current top
+    print_node(temp);
+
+    // pop top of stack and assess left- and right-child and then push them back to stack if they aren't NULL and has not yet been visited.
+    topp = pop(topp);
+    if(temp -> rchild != NULL && temp -> rchild -> visited == false)
+    {
+      topp = push(topp, temp -> rchild);
+      temp -> rchild -> visited = true;
+    }
+
+    if(temp -> lchild != NULL && temp -> lchild -> visited == false)
+    {
+      topp = push(topp, temp -> lchild);
+      temp -> lchild -> visited = true;
+    }
+  }
 }
 
 node *make_node (int num, node * left, node * right)
 {
-	return 0;
+  node *number = (node*) malloc(sizeof(node));
+  number -> num = num;
+  number -> visited = false;
+  number -> lchild = left;
+  number -> rchild = right;
+
+  return number;
 }
 
 void free_node (node * p)
 {
-	
+	if(p -> lchild == NULL && p -> rchild == NULL)
+    free(p);
+  else   // recursive step
+  {
+    free_node(p->lchild);
+    free_node(p->rchild);
+    free(p);
+  }
 }
 
 
@@ -31,7 +72,7 @@ void print_node (node * p)
   if (p == NULL)
     printf ("NULL\n");
   else
-    printf ("%d", p->num);
+    printf ("%d ", p->num);
 }
 
 
@@ -58,17 +99,28 @@ void print_tree (node * p, int depth)
 
 stack *push (stack * topp, node * node)
 {
-	return 0;
+  // precondition
+  assert(node != NULL);
+
+  // postcondition
+  struct stack * temp = (struct stack*)malloc(sizeof(struct stack*));
+  temp -> node = node;
+  temp -> next = topp; // temp becomes new topp
+  
+  return temp;
 }
 
 bool isEmpty (stack * topp)
 {
+  if (topp == NULL)
+    return true;
+  // else
   return false;
 }
 
 node *top (stack * topp)
 {
-	return 0;
+	return topp -> node;
 }
 
 // Utility function to pop topp  
@@ -76,7 +128,15 @@ node *top (stack * topp)
 
 stack *pop (stack * topp)
 {
-	return 0;
+  // precondition
+  assert(topp != NULL);
+
+  // postcondition
+  stack * temp = topp; // creating a temporary pointer which points to the soon-to-be old top
+  topp = topp -> next; // topp is assigned to next in stack
+  free (temp);
+
+	return topp;
 }
 
 void print_stack (stack * topp)
